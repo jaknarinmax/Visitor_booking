ccb98237d8ba7487847052297bf420ca8b55725e
jaknarinmax <maxlovebod@gmail.com>
2024-09-21 14:51:25 +0700

    ดีนะ

--- src/views/Training.vue
+++ src/views/Training.vue
@@ -1,43 +1,14 @@
 <template>
 
-  /* From Uiverse.io by 3bdel3ziz-T */ 
-  <div
-    class="group select-none w-[250px] flex flex-col p-4 relative items-center justify-center bg-gray-800 border border-gray-800 shadow-lg rounded-2xl"
-  >
+  /* From Uiverse.io by Javierrocadev */ 
+  <div class="w-60 h-80 bg-neutral-800 rounded-3xl text-neutral-300 p-4 flex flex-col items-start justify-center gap-3 hover:bg-gray-900 hover:shadow-2xl hover:shadow-sky-400 transition-shadow">
+    <div class="w-52 h-40 bg-sky-300 rounded-2xl"></div>
     <div class="">
-      <div class="text-center p-3 flex-auto justify-center">
-        <svg
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          class="group-hover:animate-bounce w-12 h-12 flex items-center text-gray-600 fill-red-500 mx-auto"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            clip-rule="evenodd"
-            d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-            fill-rule="evenodd"
-          ></path>
-        </svg>
-        <h2 class="text-xl font-bold py-4 text-gray-200">Are you sure?</h2>
-        <p class="font-bold text-sm text-gray-500 px-2">
-          Do you really want to continue ? This process cannot be undone
-        </p>
-      </div>
-      <div class="p-2 mt-2 text-center space-x-1 md:block">
-        <button
-          class="mb-2 md:mb-0 bg-gray-700 px-5 py-2 text-sm shadow-sm font-medium tracking-wider border-2 border-gray-600 hover:border-gray-700 text-gray-300 rounded-full hover:shadow-lg hover:bg-gray-800 transition ease-in duration-300"
-        >
-          Cancel
-        </button>
-        <button
-          class="bg-red-500 hover:bg-transparent px-5 ml-4 py-2 text-sm shadow-sm hover:shadow-lg font-medium tracking-wider border-2 border-red-500 hover:border-red-500 text-white hover:text-red-500 rounded-full transition ease-in duration-300"
-        >
-          Confirm
-        </button>
-      </div>
+        <p class="font-extrabold">Card title</p>
+        <p class="">4 popular types of cards in UI design.</p>
     </div>
+    <button class="bg-sky-700 font-extrabold p-2 px-6 rounded-xl hover:bg-sky-500 transition-colors">See more</button>
   </div>
-  
 
 
 
